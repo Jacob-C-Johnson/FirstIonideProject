0d85a0b9be1981b48fad684fd4ff8734dc124b00
Jacob Johnson <jacobj1789@gmail.com>
2024-04-04 16:51:39 -0400

    Full api made

--- Program.fs
+++ Program.fs
@@ -1,20 +1,112 @@
-﻿module PigLatin =
-    let toPigLatin (word: string) =
-        let isVowel (c: char) =
-            match c with
-            | 'a' | 'e' | 'i' | 'o' | 'u'
-            | 'A' | 'E' | 'I' | 'O' | 'U' -> true
-            |_ -> false
-        
-        if isVowel word[0] then
-            word + "yay"
-        else
-            word[1..] + string word[0] + "ay"
+﻿open Suave
+open Suave.Operators
+open Suave.Filters
+open Suave.Successful
+open System
+open Newtonsoft.Json
+open Suave.Writers
+
+//Create custom type to hold timezone information
+type TZInfo = {tzName: string; minDiff: float; localTime: string; utcOffset: float}
+
+// Function for closest timezone
+let getClosest () = 
+
+    // This gets all the time zones into a List-like object
+
+    let tzs = TimeZoneInfo.GetSystemTimeZones()
+
+    // List comprehension + type inference allows us to easily perform conversions
+
+    let tzList = [
+
+        for tz in tzs do
+
+        // convert the current time to the local time zone
+
+        let localTz = TimeZoneInfo.ConvertTime(DateTime.Now, tz) 
+
+        // Get the datetime object if it was 5:00pm 
+
+        let fivePM = DateTime(localTz.Year, localTz.Month, localTz.Day, 17, 0, 0)
+
+        // Get the difference between now local time and 5:00pm local time.
+
+        let minDifference = (localTz - fivePM).TotalMinutes
+
+        yield {
+
+                tzName=tz.StandardName;
+
+                minDiff=minDifference;
+
+                localTime=localTz.ToString("hh:mm tt");
+
+                utcOffset=tz.BaseUtcOffset.TotalHours;
+
+             }
+
+    ]
+
+    // We use the pipe operator to chain function calls together
+
+    tzList 
+
+        // filter so that we only get tz after 5pm
+
+        |> List.filter (fun (i:TZInfo) -> i.minDiff >= 0.0) 
+
+        // sort by minDiff
+
+        |> List.sortBy (fun (i:TZInfo) -> i.minDiff) 
+
+        // Get the first item
+
+        |> List.head
+
+// We'll use argv later :)
+let runWebServer argv = 
+
+    // Define the port where you want to serve. We'll hardcode this for now.
+
+    let port = 8080
+
+    // create an app config with the port
+
+    let cfg =
+
+          { defaultConfig with
+
+              bindings = [ HttpBinding.createSimple HTTP "0.0.0.0" port]}
+
+    // We'll define a single GET route at the / endpoint that returns "Hello World"
+
+    let app =
+
+          choose
+
+            [ GET >=> choose
+
+                [ 
+
+                    // We are getting the closest time zone, converting it to JSON, then setting the MimeType
+
+                    path "/" >=> request (fun _ -> OK <| JsonConvert.SerializeObject(getClosest()))
+
+                                >=> setMimeType "application/json; charset=utf-8"
+
+                ]
+
+            ]
+
+    // Now we start the server
+
+    startWebServer cfg app
 
 [<EntryPoint>]
-let main args =
-    for arg in args do
-        let newArg = PigLatin.toPigLatin arg
-        printfn "%s in Pig Latin is: %s" arg newArg
+
+let main argv = 
+
+    printfn "%s" <| JsonConvert.SerializeObject(getClosest())
 
     0
--- obj/Debug/net8.0/FirstIonideProject.AssemblyInfo.fs
+++ obj/Debug/net8.0/FirstIonideProject.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a130b71f74d2a8f742e4fb3c1239f45d55fb9f4")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26991fa9ec37fdfa2a583822c382b7781bbecb6f")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
