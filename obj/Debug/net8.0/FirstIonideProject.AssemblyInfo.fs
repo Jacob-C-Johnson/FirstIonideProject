956736d00e964129e6da35313c4e81552c6f531d
Jacob Johnson <jacobj1789@gmail.com>
2024-04-04 16:58:08 -0400

    no merege conflict allowed

--- obj/Debug/net8.0/FirstIonideProject.AssemblyInfo.fs
+++ obj/Debug/net8.0/FirstIonideProject.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26991fa9ec37fdfa2a583822c382b7781bbecb6f")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
