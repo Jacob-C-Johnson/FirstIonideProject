0c55716da728769b52946750eaafb8c0fcd760d5
Jacob-C-Johnson <jacobj1789@gmail.com>
2024-04-08 12:36:13 -0400

    Laptop run

--- obj/Debug/net8.0/FirstIonideProject.AssemblyInfo.fs
+++ obj/Debug/net8.0/FirstIonideProject.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eed833cca46d8d3416fca0d2a5f7ab850b74cf1")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
